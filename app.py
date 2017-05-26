eca0a92a807a9f8c111cd3f25fd4c24172485918
Matthew Gall <me@matthewgall.com>
2017-05-26 22:48:48 +0100

    Initial commit of current working copy

---
+++ app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys, os, logging, json, ntplib
+from bottle import route, request, response, redirect, hook, error, default_app, view, static_file, template, HTTPError
+from time import ctime
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+	return static_file(filepath, root='views/static')
+
+@route('/')
+def index():
+	c = ntplib.NTPClient()
+	response = c.request(os.getenv('APP_SERVER', 'localhost'), version=3)
+	return "{}<br/>{}".format(ctime(response.tx_time), response.offset)
+
+if __name__ == '__main__':
+	app = default_app()
+
+	serverHost = os.getenv('IP', 'localhost')
+	serverPort = os.getenv('PORT', '5000')
+
+	# Now we're ready, so start the server
+	# Instantiate the logger
+	log = logging.getLogger('log')
+	console = logging.StreamHandler()
+	log.setLevel(logging.INFO)
+	log.addHandler(console)
+
+	# Now we're ready, so start the server
+	try:
+		app.run(host=serverHost, port=serverPort, server='tornado')
+	except:
+		log.error("Failed to start application server")
