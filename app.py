41b5be48b7357baa8b8c3230c26e5dbd214081aa
Matthew Gall <me@matthewgall.com>
2017-11-13 22:50:57 +0000

    Added support for /.well-known/time for NTP over HTTP support, CORS and other goodies!

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
 
-import sys, os, logging, json, ntplib, argparse
+import sys, os, logging, json, ntplib, argparse, time
 from bottle import route, request, response, redirect, hook, error, default_app, view, static_file, template, HTTPError
 from time import ctime
+
+def set_content_type(fn):
+	def _return_json(*args, **kwargs):
+		response.headers['Content-Type'] = 'application/json'
+		if request.method != 'OPTIONS':
+			return fn(*args, **kwargs)
+	return _return_json
+
+def enable_cors(fn):
+	def _enable_cors(*args, **kwargs):
+		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+		response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+		if request.method != 'OPTIONS':
+			return fn(*args, **kwargs)
+	return _enable_cors
+
+def embed_date(fn):
+	def _embed_date(*args, **kwargs):
+		response.set_header('Date', time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.gmtime(fetch_time().tx_time)))
+		if request.method != 'OPTIONS':
+			return fn(*args, **kwargs)
+	return _embed_date
 
 def fetch_time():
 	try:
 		c = ntplib.NTPClient()
-		return c.request(os.getenv('APP_SERVER', 'localhost'), version=3)
+		return c.request(args.server, version=3)
 	except NTPException:
 		return False
 
@@ -20,10 +44,16 @@ def static_favicon():
 	response.content_type = 'image/x-icon'
 	return static_file("favicon.ico", root='views/static')
 
+@route("/.well-known/time", ('HEAD', 'GET'))
+@embed_date
+def get_ntphttp():
+	return response.get_header('Date')
+
 @route('/api/update')
+@enable_cors
 def get_delay():
 	data = {
-		"timestamp": "{} UTC".format(ctime(fetch_time().tx_time)),
+		"timestamp": "{} UTC".format(time.gmtime(fetch_time().tx_time)),
 		"offset": "{}s".format(format(fetch_time().offset, '.15f')),
 		"delay": "{}s".format(format(fetch_time().delay, '.15f'))
 	}
@@ -31,8 +61,9 @@ def get_delay():
 	return json.dumps(data)
 
 @route('/')
+@enable_cors
 def index():
-	return template("index", host=os.getenv('APP_SERVER', 'localhost'), response=fetch_time())
+	return template("index", host=args.server, response=fetch_time())
 
 if __name__ == '__main__':
 
@@ -41,6 +72,9 @@ if __name__ == '__main__':
 	# Server settings
 	parser.add_argument("-i", "--host", default=os.getenv('IP', '127.0.0.1'), help="IP Address")
 	parser.add_argument("-p", "--port", default=os.getenv('PORT', 5000), help="Port")
+
+	# Time server
+	parser.add_argument("-s", "--server", default=os.getenv('APP_SERVER', 'localhost'), help="address of NTP server")
 
 	# Verbose mode
 	parser.add_argument("--verbose", "-v", help="increase output verbosity", action="store_true")
